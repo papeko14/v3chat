de69fbcf69729bdd7574062ac82ac853e7b9d2ac
thanopath <44945805+papeko14@users.noreply.github.com>
2025-08-25 22:22:37 +0700

    Update chat_AI.py

--- chat_AI.py
+++ chat_AI.py
@@ -7,14 +7,15 @@ st.set_page_config(page_title="n8n Chatbot App", layout="centered")
 # --- Sidebar for Navigation ---
 # สร้างเมนูทางด้านซ้าย (Sidebar)
 #Data = st.Page(page='views/testdata.py',title='Data', icon='📅')
-chat_bot = st.Page(page='v3chat/views/Chat_page.py',title='Chat_bot', icon='🤖')
-chat_zone = st.Page(page='v3chat/views/Chat_page_zone.py',title='Chat_bot_zone',icon='🤖')
+chat_bot = st.Page(page='views/Chat_page.py',title='Chat_bot', icon='🤖')
+chat_zone = st.Page(page='views/Chat_page_zone.py',title='Chat_bot_zone',icon='🤖')
 #graph = st.Page(page='v3chat/views/Graph_data.py',title='graph', icon='📊')
 #test_data = st.Page(page='v3chat/views/Table_Data.py',title='Table_Data', icon='📊')
 pg = st.navigation(
         pages=[chat_bot,chat_zone]
     )
 pg.run()
+
 
 
 
